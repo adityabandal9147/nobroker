3638180a63969e5cb11db085144a008c34740dd2
aditya <adityabandal12@gmail.com>
2024-01-28 02:40:15 -0800

    27 th jan 2024

--- nobroker/src/main/java/com/nobroker/entity/User.java
+++ nobroker/src/main/java/com/nobroker/entity/User.java
@@ -1,32 +1,26 @@
 package com.nobroker.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+import lombok.*;
+
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
 
-    public long getId() {
-        return id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        private String email;
+        private String password;
+        private String mobile;
+        private boolean emailVerified;
+
+
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    private String name;
-    private  String email;
-    private String mobile;
-    private String password;
-}
